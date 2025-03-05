be655e124a400c6a1fc84a8faeaf4be902772af0
nkumarreddy <naveen.t@s.amity.edu>
2025-03-05 23:09:25 +0530

    updated

--- file1.py
+++ file1.py
@@ -37,3 +37,21 @@ print(fact)i
 celsius = float(input("Enter temperature in Celsius: "))
 fahrenheit = (celsius * 9/5) + 32
 print(fahrenheit)
+
+
+#banking 
+pin = 1234
+attempts = 0
+
+while attempts < 3:
+    a = int(input("Enter the PIN: "))
+
+    if pin == a:
+        print("Valid")
+        break
+    else:
+        print("Invalid")
+        attempts += 1
+
+if attempts == 3:
+    print("Try later after 24 hours")
